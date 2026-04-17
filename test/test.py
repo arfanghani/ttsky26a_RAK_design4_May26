7454ff6c45369b93995100fb105c33f7df1014f3
ag <76513830+arfanghani@users.noreply.github.com>
2026-04-17 23:48:07 +0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,37 +2,28 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 
+def safe_int(sig):
+    val = sig.value
+    if "x" in val.binstr.lower() or "z" in val.binstr.lower():
+        return 0
+    return val.integer
+
 @cocotb.test()
 async def cpu_test(dut):
 
     clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
-    # --------------------
-    # INIT
-    # --------------------
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-
-    # --------------------
-    # RESET
-    # --------------------
     dut.rst_n.value = 0
-    for _ in range(5):
-        await RisingEdge(dut.clk)
+    dut.ena.value = 1
 
+    await RisingEdge(dut.clk)
     dut.rst_n.value = 1
 
-    for _ in range(5):
-        await RisingEdge(dut.clk)
-
-    # --------------------
-    # RUN CPU
-    # --------------------
-    for i in range(80):
+    for _ in range(120):
 
         await RisingEdge(dut.clk)
 
-        pc = int(dut.uio_out.value)  # ONLY if you exposed PC
-        dut._log.info(f"cycle={i} PC={pc}")
+        pc = safe_int(dut.uio_out)
+
+        dut._log.info(f"PC={pc}")
